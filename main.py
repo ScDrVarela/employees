21ce8c9a894dd9860a41b38406a3f323db2581ff
scdrvarela <scdrvarela@gmail.com>
2023-01-04 15:18:09 -0700

    Just add

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+
+This project will be used to connect to a local MySQL DB and create script which will allow the user to create tables
+within a specific DB, edit tables with actions such as drop tables
+
+"""
