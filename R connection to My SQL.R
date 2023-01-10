a87d7eccd1a601b22652461acd33157c343ef403
scdrvarela <scdrvarela@gmail.com>
2023-01-10 16:43:01 -0700

    This R script will stablish the connection between R and MySQL DB

---
+++ R connection to My SQL.R
@@ -0,0 +1,8 @@
+#Te first lines will stablish the connection with MySQL Database
+
+mysqlconnection = dbConnect(RMySQL::MySQL(),
+                            dbname = 'ferresteel',
+                            host = 'localhost',
+                            port = 3306,
+                            user = 'root',
+                            password = 'Ferrestill2712')
